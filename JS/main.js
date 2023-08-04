a77312f7e977424e199c9531c3bbf375f598d92f
rakeem@DESKTOP-TKIBL4I <rakeem@DESKTOP-TKIBL4I>
2023-08-04 15:24:25 -0700

    small edits

--- JS/main.js
+++ JS/main.js
@@ -1,3 +1,87 @@
+const portfolioInfo = [
+  {
+    id: "web-1",
+    type: "web",
+    img: "./assets/images/portfolio-1.jpg",
+    category: "Web Development",
+    title: "Feature Project 1",
+    description: "My first awesome website",
+    detail: "This website was developed to demonstrate mastery over HTML and CSS",
+    link: "https://chapmans-web.netlify.app/"
+  },
+  {
+    id: "web-2",
+    type: "web",
+    img: "./assets/images/portfolio-2.jpg",
+    category: "Web Development",
+    title: "Feature Project 2",
+    description: "My Awesome Website",
+    detail: "This website demonstrates additional skills that weren't showcased before.",
+    link: "https://chapmanfront.vercel.app"
+  },
+  {
+    id: "web-3",
+    type: "web",
+    img: "./assets/images/portfolio-3.jpg",
+    category: "Web Development",
+    title: "Feature Project 3",
+    description: "My Awesome Website",
+    detail: "This website demonstrates Javascript proficiency as well as more advanced CSS",
+    link: "https://chapman-world-wide.vercel.app"
+  },
+  {
+    id: "web-4",
+    type: "web",
+    img: "./assets/images/portfolio-4.jpg",
+    category: "Web Development",
+    title: "Path To Legacy",
+    description: "My awesome webapp",
+    detail: "This a InstaCart styled website (V1) that's used to enable customers to purchase their favorite snack and have it shipped directly to them",
+    link: "https://path-to-legacy-ghfp-git-main-rakeem-c.vercel.app/?vercelToolbarCode=RhLAqHJArCVLgqN"
+  },
+  {
+    id: "ui-2",
+    type: "ui",
+    img: "./assets/images/portfolio-5.jpg",
+    category: "Web Development",
+    title: "Feature Project 5",
+    description: "My awesome webapp",
+    detail: "My next masterpiece...coming soon!",
+    link: "https://chapman-world-wide.vercel.app"
+  },
+  {
+    id: "app-1",
+    type: "app",
+    img: "./assets/images/portfolio-5.jpg",
+    category: "Web Development",
+    title: "Feature Project 6",
+    description: "My awesome webapp",
+    detail: "My next masterpiece...coming soon!",
+    link: "https://chapman-world-wide.vercel.app"
+  },
+  {
+    id: "app-2",
+    type: "app",
+    img: "./assets/images/portfolio-5.jpg",
+    category: "Web Development",
+    title: "Feature Project 7",
+    description: "My awesome webapp",
+    detail: "My next masterpiece...coming soon!",
+    link: "https://chapman-world-wide.vercel.app"
+  },
+  {
+    id: "web-5",
+    type: "web",
+    img: "./assets/images/portfolio-5.jpg",
+    category: "Web Development",
+    title: "Feature Project 8",
+    description: "My awesome webapp",
+    detail: "My next masterpiece...coming soon!",
+    link: "https://chapman-world-wide.vercel.app"
+  },
+  
+  // Add more portfolio items here...
+];
 const theme = 'theme';
 const dataTheme = 'data-theme';
 const themeTab = '.theme-tab';
@@ -47,6 +131,76 @@ const setTheme = (val) => {
   }
 };
 
+const portfolioGrid = document.querySelector('.portfolio-grid');
+
+// Fill the portfolioHtml and append it to the portfolioGrid
+let portfolioHtml = '';
+for (let itemData of portfolioInfo) {
+  portfolioHtml += `
+    <div class="portfolio-card" data-item="${itemData.type}" data-open="${itemData.id}">
+      <div class="card-body">
+        <img src="${itemData.img}" alt="portfolio icon">
+        <div class="card-popup-box">
+          <div>${itemData.category}</div>
+          <h3>${itemData.title}</h3>
+        </div>
+      </div>
+    </div>
+  `;
+}
+// Append the new portfolio cards to the portfolio grid
+portfolioGrid.innerHTML = portfolioHtml;
+// Event listener for when a portfolio card is clicked
+document.querySelector('.portfolio-grid').addEventListener('click', function(e) {
+  let target = e.target;
+
+  // Traverse up in case of inner elements clicked within the portfolio card
+  while(target != this && !target.classList.contains('portfolio-card')) {
+    target = target.parentElement;
+  }
+
+  // Check if a portfolio card was clicked
+  if(target.classList.contains('portfolio-card')) {
+    const itemId = target.dataset.open;
+    const itemData = portfolioInfo.find(item => item.id === itemId);
+
+    // Check if matching data for the clicked card was found
+    if (itemData) {
+      const modalHtml = `
+        <div id="${itemData.id}" class="modal is-visible" data-animation="slideInOutTop">
+          <div class="modal-dialog">
+            <header class="modal-header">
+              <h3>${itemData.title}</h3>
+              <i class="fas fa-times" data-close></i>
+            </header>
+            <div class="modal-body">
+              <div class="img-wrapper">
+                <img src="${itemData.img}" alt="portfolio image">
+              </div>
+              <div class="text-wrapper">
+                <p><strong>${itemData.description}</strong></p>
+                <p>${itemData.detail}</p>
+                <a href="${itemData.link}">Check it out!</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      `;
+
+      // Append the new modal to the site wrapper
+      document.querySelector('.site-wrapper').insertAdjacentHTML('beforeend', modalHtml);
+    }
+  }
+});
+
+// Event listener for when a modal close button is clicked
+document.querySelector('.site-wrapper').addEventListener('click', function(e) {
+  if (e.target.classList.contains('fa-times')) {
+    e.target.closest('.modal').remove();
+  }
+});
+
+
 if (currentTheme) {
   root.setAttribute(dataTheme, currentTheme);
   switcher.forEach((btn) => {
@@ -79,16 +233,16 @@ for (const elm of switcher) {
 }
 
 searchBox.addEventListener('keyup', (e) => {
-const searchInput = e.target.value.toLowerCase().trim();
+  const searchInput = e.target.value.toLowerCase().trim();
+  const portfolioCards = portfolioGrid.querySelectorAll('.portfolio-card');
 
-portfolioItems.forEach((card) => {
-  if (card.dataset.item.includes(searchInput)) {
-    card.style.display ='block';
-  } else {
-    card.style.display = 'none';
-  }
-})
-
+  portfolioCards.forEach((card) => {
+    if (card.dataset.item.includes(searchInput)) {
+      card.style.display ='block';
+    } else {
+      card.style.display = 'none';
+    }
+  })
 });
 
 
@@ -147,3 +301,4 @@ for (let i = 0; i < elmsDisplayed; i += 1) {
 // get elements displayed
 //nodelist.length
 // assign --marquee-elms nodelist.length
+
